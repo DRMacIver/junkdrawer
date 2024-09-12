4e9fe45e1ef441c82e9213730f855d61d1d9b4ea
David R. MacIver <david@drmaciver.com>
2024-09-12 11:45:08 +0100

    Handle dirty clauses more efficiently when multiple units are added at once

--- src/drmaciver_junkdrawer/sat/unitpropagator.py
+++ src/drmaciver_junkdrawer/sat/unitpropagator.py
@@ -1,4 +1,4 @@
-from collections import defaultdict, deque
+from collections import defaultdict
 from typing import Iterable
 
 
@@ -13,24 +13,14 @@ class UnitPropagator:
         self.__watched_by = [set() for _ in self.__clauses]
 
         self.units = set()
-        self.__unit_queue = deque()
-
-        for i, clause in enumerate(self.__clauses):
-            if not clause:
-                raise Inconsistent("Clauses contain an empty clause")
-            if len(clause) == 1:
-                self.__enqueue_unit(clause[0])
-            else:
-                self.__watches[clause[0]].add(i)
-                self.__watches[clause[1]].add(i)
-                self.__watched_by[i].update(clause[:2])
-        self.__process_queue()
+        self.__dirty = set(range(len(clauses)))
+        self.__clean_dirty_clauses()
 
     def add_units(self, units: Iterable[int]) -> None:
-        assert not self.__unit_queue, self.__unit_queue
+        assert not self.__dirty, self.__dirty
         for u in units:
             self.__enqueue_unit(u)
-        self.__process_queue()
+        self.__clean_dirty_clauses()
 
     def add_unit(self, unit: int) -> None:
         self.add_units((unit,))
@@ -42,40 +32,35 @@ class UnitPropagator:
                     f"Tried to add {unit} as a unit but {-unit} is already a unit."
                 )
             self.units.add(unit)
-            self.__unit_queue.append(unit)
+            self.__dirty.update(self.__watches.pop(-unit, ()))
 
-    def __process_queue(self):
-        processed = set()
-        while self.__unit_queue:
-            new_unit = self.__unit_queue.popleft()
-            processed.add(new_unit)
-            self.__watches.pop(new_unit, None)
-            killed = -new_unit
-            need_checking = self.__watches.pop(killed, ())
-            for i in need_checking:
-                self.__watched_by[i].remove(killed)
-                (remaining_watch,) = self.__watched_by[i]
-                if remaining_watch in self.units:
+    def __clean_dirty_clauses(self):
+        while self.__dirty:
+            dirty = self.__dirty
+            self.__dirty = set()
+
+            for i in dirty:
+                clause = self.__clauses[i]
+                if not clause:
+                    raise Inconsistent("Clauses contain an empty clause")
+                if any(literal in self.units for literal in clause):
+                    for literal in self.__watched_by[i]:
+                        if literal in self.__watches:
+                            self.__watches[literal].remove(i)
                     self.__watched_by[i].clear()
-                    continue
-                for literal in self.__clauses[i]:
-                    if literal == remaining_watch:
-                        continue
-                    if literal in self.units:
-                        # If this clause is already satisfied we don't need to
-                        # watch it any more
-                        self.__watches[remaining_watch].remove(i)
-                        self.__watched_by[i].clear()
-                        break
-                    elif -literal not in self.units:
-                        # Otherwise, we need to find some new literal in the
-                        # clause to watch it from.
-                        self.__watched_by[i].add(literal)
-                        self.__watches[literal].add(i)
-                        break
                 else:
-                    # remaining_watch is the only literal left in this
-                    # clause, so must be a unit.
-                    self.__enqueue_unit(remaining_watch)
-        assert not self.__unit_queue
-        return processed
+                    for literal in list(self.__watched_by[i]):
+                        if -literal in self.units:
+                            self.__watched_by[i].remove(literal)
+                    for literal in clause:
+                        if len(self.__watched_by[i]) == 2:
+                            break
+                        if -literal not in self.units:
+                            self.__watches[literal].add(i)
+                            self.__watched_by[i].add(literal)
+                    if len(self.__watched_by[i]) == 0:
+                        raise Inconsistent(
+                            f"Clause {' '.join(map(str, clause))} can no longer be satisfied"
+                        )
+                    elif len(self.__watched_by[i]) == 1:
+                        self.__enqueue_unit(*self.__watched_by[i])
